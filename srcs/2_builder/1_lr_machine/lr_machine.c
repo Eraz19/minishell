c6295212da0653176e9417a25c32ad71cef24586
SudoQuack <guillaume.astesana@gmail.com>
2026-05-05 16:28:01 +0200

    LR(1) lr_machine READY (20s compute) 🚀🚀🚀

--- srcs/2_builder/1_lr_machine/lr_machine.c
+++ srcs/2_builder/1_lr_machine/lr_machine.c
@@ -7,6 +7,7 @@
 #include "action.h"
 #include "goto.h"
 #include <stdlib.h>
+#include <locale.h>
 
 void	lr_machine_init(t_lr_machine *machine)
 {
@@ -19,24 +20,26 @@ void	lr_machine_init(t_lr_machine *machine)
 
 bool	lr_machine_build(t_lr_machine *machine)
 {
+	setlocale(LC_NUMERIC, "de_DE");
 	printf("lr_machine_build: START\n");
 	rules_build(machine->rules);
-	printf("lr_machine_build: rules table built            (%i entries)\n", RULE_COUNT);
+	printf("lr_machine_build: rules table built            (entries: %'6i)\n", RULE_COUNT);
 	symbols_build_nullables_table(machine);
-	printf("lr_machine_build: nullable symbols table built (%i entries)\n", SYM_COUNT);
+	printf("lr_machine_build: nullable symbols table built (entries: %'6i)\n", SYM_COUNT);
 	first_build_table(machine);
-	printf("lr_machine_build: first table built            (%i entries)\n", SYM_COUNT * (SYM_TERMINAL_MAX + 1));
+	printf("lr_machine_build: first table built            (entries: %'6i)\n", SYM_COUNT * (SYM_TERMINAL_MAX + 1));
 	if (!transition_build_table(machine))
 		return (false);
-	printf("lr_machine_build: transitions table built      (%zu entries)\n", machine->transitions.len);
-	printf("lr_machine_build: lr_states table built        (%zu entries)\n", machine->lr_states.len);
+	printf("lr_machine_build: transitions table built      (entries: %'6zu)\n", machine->transitions.len);
+	printf("lr_machine_build: lr_states table built        (entries: %'6zu)\n", machine->lr_states.len);
 	if (!goto_build_table(machine))
 		return (false);
-	printf("lr_machine_build: gotos table built            (%zu entries)\n", machine->lr_states.len * (SYM_NON_TERMINAL_MAX - SYM_NON_TERMINAL_MIN + 1));
+	printf("lr_machine_build: gotos table built            (entries: %'6zu)\n", machine->lr_states.len * (SYM_NON_TERMINAL_MAX - SYM_NON_TERMINAL_MIN + 1));
 	if (!action_build_table(machine))
 		return (false);
-	printf("lr_machine_build: actions table built          (%zu entries)\n", machine->lr_states.len * (SYM_TERMINAL_MAX + 1));
+	printf("lr_machine_build: actions table built          (entries: %'6zu)\n", machine->lr_states.len * (SYM_TERMINAL_MAX + 1));
 	printf("lr_machine_build: DONE\n");
+	setlocale(LC_NUMERIC, "");
 	return (true);
 }
 
