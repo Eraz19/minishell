fe72c53af265322239f671a75af188f484c1a8a5
SudoQuack <guillaume.astesana@gmail.com>
2026-05-05 14:18:01 +0200

    update transition module of lr_machine to LR(1) version

--- srcs/2_builder/1_lr_machine/7_transition/2_core.c
+++ srcs/2_builder/1_lr_machine/7_transition/2_core.c
@@ -1,6 +1,6 @@
 #include "lr_state.h"
 
-static bool	build_initial_lr_state(t_rule *rules, t_lr_state *lr_states)
+static bool	build_initial_lr_state(t_lr_machine *machine, t_lr_state *lr_states)
 {
 	t_rule_state	rule_state;
 	t_lr_state		lr_state;
@@ -8,9 +8,10 @@ static bool	build_initial_lr_state(t_rule *rules, t_lr_state *lr_states)
 	lr_state_init(&lr_state);
 	rule_state.rule_id = RULE_START_1;
 	rule_state.pos = 0;
+	rule_state.lookahead = SYM_EOF;
 	if (!lr_state_add_rule_state(&lr_state, rule_state, NULL))
 		return (lr_state_free(&lr_state), false);
-	if (!lr_state_complete(rules, &lr_state))
+	if (!lr_state_complete(machine, &lr_state))
 		return (lr_state_free(&lr_state), false);
 	if (!lr_state_add(lr_states, &lr_state, NULL))
 		return (lr_state_free(&lr_state), false);
@@ -38,7 +39,7 @@ static bool	build_lr_state_and_transition(t_lr_machine *machine, t_lr_state *lr_
 	t_lr_state	lr_state_new;
 	bool		did_add;
 
-	if (!lr_state_next(machine->rules, &lr_state_new, lr_state_from, symbol))
+	if (!lr_state_next(machine, &lr_state_new, lr_state_from, symbol))
 		return (false);
 	if (lr_state_new.len > 0)
 	{
@@ -63,7 +64,7 @@ bool	transition_build_table(t_lr_machine *machine)
 	t_symbol	symbol;
 	t_lr_state	lr_state;
 
-	if (!build_initial_lr_state(machine->rules, &machine->lr_states))
+	if (!build_initial_lr_state(machine, &machine->lr_states))
 		return (false);
 	i = 0;
 	while (i < machine->lr_states.len)
