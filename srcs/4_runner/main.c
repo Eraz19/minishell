ced50127fb2dcbe999063daa48c397c7ad4f6cf9
SudoQuack <guillaume.astesana@gmail.com>
2026-06-27 03:12:38 +0200

    feat(runner): reset scanner + builder on syntax errors (scanner bugged)

--- srcs/4_runner/main.c
+++ srcs/4_runner/main.c
@@ -2,6 +2,7 @@
 #include "runner.h"
 #include "builder.h"
 #include "options.h"
+#include "shell.h"
 #include <stdbool.h>
 
 void	runner_init(t_runner *runner)
@@ -10,12 +11,26 @@ void	runner_init(t_runner *runner)
 	// TODO;
 }
 
-static bool	runner_continue_on_builder_error(t_runner *runner, t_error err)
+static inline t_error	runner_handle_builder_error(t_runner *runner, t_error err)
 {
+	t_scanner	*scanner;
+
 	(void)runner;
-	(void)err;
 	// TODO
-	return (false);
+	if (err.type != ERR_SYNTAX_INVALID)
+		return (err);
+	scanner = shell_get_scanner();
+	if (!scanner)
+		return (error(ERR_SHELL_NOT_FOUND));
+	err = scanner_reset(scanner);
+	if (err.type)
+		return (error_print(err, "runner", "unable to reset scanner",
+			NULL, NULL));
+	err = builder_reset();
+	if (err.type)
+		return (error_print(err, "runner", "unable to reset builder",
+			NULL, NULL));
+	return (err);
 }
 
 t_error	runner_loop_cycle(t_runner *runner)
@@ -24,13 +39,16 @@ t_error	runner_loop_cycle(t_runner *runner)
 	t_error		err;
 
 	err = builder_get_ast(&ast_root);
-	if (err.type != ERR_NO && !runner_continue_on_builder_error(runner, err))
-		return (err);
-	ast_root_free(&ast_root);
+	if (err.type != ERR_NO)
+	{
+		err = runner_handle_builder_error(runner, err);
+		if (err.type)
+			return (ast_root_free(&ast_root), err);
+	}
 	// TODO: err = walk_ast(&ast_root);
-	// TODO: handle non-fatal errors
-	// TODO: ast_free_node(&ast_root);
-	return (error(ERR_NO));
+	// TODO: err = runner_handle_walker_error(runner, err);
+	ast_root_free(&ast_root);
+	return (err);
 }
 
 t_error	runner_run(t_runner *runner)
