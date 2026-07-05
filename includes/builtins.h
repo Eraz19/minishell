a2a12ea59cf24a34b879e094359195723bfc487a
SudoQuack <guillaume.astesana@gmail.com>
2026-07-06 01:49:16 +0200

    feat(builtins): add all builtins placeholders (needed for runner implementation)

--- includes/builtins.h
+++ includes/builtins.h
@@ -3,26 +3,49 @@
 
 # include <stdbool.h>
 
-typedef int (*t_builtin_function)(int argc, char **argv, char **envp);
+typedef int (*t_builtin)(int argc, char **argv, char **envp);
 
-typedef struct s_builtin
-{
-	const char			*name;
-	t_builtin_function	function;
-}	t_builtin;
+// Special builtins (Shell shall exit on error if invoked directly && non-interactive)
 
-/* ************************************************************************* */
-/*                                   GETTER                                  */
-/* ************************************************************************* */
+int	builtin_break(int argc, char **argv, char **envp);
+int	builtin_colon(int argc, char **argv, char **envp);
+int	builtin_continue(int argc, char **argv, char **envp);
+int	builtin_dot(int argc, char **argv, char **envp);
+int	builtin_eval(int argc, char **argv, char **envp);
+int	builtin_exec(int argc, char **argv, char **envp);
+int	builtin_exit(int argc, char **argv, char **envp);
+int	builtin_export(int argc, char **argv, char **envp);
+int	builtin_readonly(int argc, char **argv, char **envp);
+int	builtin_return(int argc, char **argv, char **envp);
+int	builtin_set(int argc, char **argv, char **envp);
+int	builtin_shift(int argc, char **argv, char **envp);
+int	builtin_times(int argc, char **argv, char **envp);
+int	builtin_trap(int argc, char **argv, char **envp);
+int	builtin_unset(int argc, char **argv, char **envp);
 
-/*
-SPECIALS:
-	- Searched first during Command Search.
-	- Shell exits on error (if invoked directly && shell is non-interactive).
-REGULAR / INTRINSICS:
-	- Searched right after functions during Command Search.
-	- Shell does not exit on error.
-*/
-t_builtin_function	builtins_get(const char *name, bool special);
+// Intrinsic builtins (Shell shall not exit on error)
+
+int	builtin_alias(int argc, char **argv, char **envp);
+int	builtin_bg(int argc, char **argv, char **envp);
+int	builtin_cd(int argc, char **argv, char **envp);
+int	builtin_command(int argc, char **argv, char **envp);
+int	builtin_fc(int argc, char **argv, char **envp);
+int	builtin_fg(int argc, char **argv, char **envp);
+int	builtin_getopts(int argc, char **argv, char **envp);
+int	builtin_hash(int argc, char **argv, char **envp);
+int	builtin_jobs(int argc, char **argv, char **envp);
+int	builtin_kill(int argc, char **argv, char **envp);
+int	builtin_read(int argc, char **argv, char **envp);
+int	builtin_type(int argc, char **argv, char **envp);
+int	builtin_ulimit(int argc, char **argv, char **envp);
+int	builtin_umask(int argc, char **argv, char **envp);
+int	builtin_unalias(int argc, char **argv, char **envp);
+int	builtin_wait(int argc, char **argv, char **envp);
+
+// Regular builtins (Shell shall not exit on error)
+
+int	builtin_echo(int argc, char **argv, char **envp);
+int	builtin_env(int argc, char **argv, char **envp);
+int	builtin_pwd(int argc, char **argv, char **envp);
 
 #endif
--- srcs/TODO.c
+++ srcs/TODO.c
@@ -1,6 +1,7 @@
 #include "error.h"
 #include "token.h"
 #include <stdbool.h>
+# include <stdio.h>	// DEBUG
 
 /* ---------- TODO: shell ---------- */
 
@@ -21,4 +22,264 @@ t_error	runner_find_cmd_sub_closing(t_token *out, t_string *input)
 	(void)out;
 	(void)input;
 	return (error(ERR_NOT_IMPLEMENTED));
+}
+
+/* ---------- TODO: builtins ---------- */
+
+// Special builtins
+int	builtin_break(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_colon(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_continue(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_dot(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_eval(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_exec(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_exit(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_return(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_set(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_shift(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_times(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_trap(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+
+// Intrinsic builtins
+
+int	builtin_alias(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_bg(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_cd(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_command(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_fc(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_fg(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_getopts(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_hash(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_jobs(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_kill(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_read(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_type(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_ulimit(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_umask(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_unalias(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+int	builtin_wait(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+
+// Regular builtins
+
+int	builtin_echo(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+
+int	builtin_env(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
+}
+
+int	builtin_pwd(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	fprintf(stderr, "%s not implemented yet.\n", __func__);
+	return (0);
 }
