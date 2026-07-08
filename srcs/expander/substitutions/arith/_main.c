fe8b72635d067395a241ca3aa16a361280b7781b
Alexander Douieb <alexanderdouieb19@gmail.com>
2026-07-08 13:51:40 +0200

    wip before starting implementing arith expansion

--- srcs/expander/substitutions/arith/_main.c
+++ srcs/expander/substitutions/arith/_main.c
@@ -1,3 +1,4 @@
+#include "expansion_.h"
 #include "arith_substitution_.h"
 
 bool	is_arith_substitution(t_word_item *current_item, uint flags)
@@ -9,8 +10,57 @@ bool	is_arith_substitution(t_word_item *current_item, uint flags)
 	return (false);
 }
 
-t_error	arith_substitution(t_expander *expander)
+t_error	extract_arith_word(t_expander *expander, t_word *word)
+{
+	size_t		i;
+	size_t		len;
+	t_word_item	item;
+
+	expander->err = word_get(&item, &expander->word, 0);
+	if (expander->err.type)
+		return (expander->err);
+	len = item.opt.context_len;
+	i = 0;
+	word_init(word);
+	while (i < len)
+	{
+		if (i > 2 && i < len - 2)
+			expander->err = forward_word_item(word, &expander->word);
+		else
+			expander->err = word_fpop(&item, &expander->word);
+		if (expander->err.type)
+			return (word_free(word), expander->err);
+		++i;
+	}
+	return (expander->err);
+}
+
+t_error	compute_arith_expression(t_expander *expander, t_expansion *expansion)
 {
 	(void)expander;
+	(void)expansion;
 	return (expander->err);
+}
+
+t_error	arith_substitution(t_expander *expander)
+{
+	t_expansion	expansion;
+	t_word		arith_word;
+	t_expander	arith_expander;
+
+	if (extract_arith_word(expander, &arith_word).type)
+		return (expander->err);
+	expander_init(&arith_expander);
+	arith_expander.word = arith_word;
+	arith_expander.ifs = expander->ifs;
+	arith_expander.flags = EXP_PARAM | EXP_CMD_SUB | EXP_QUOTE_REMOVAL;
+	expander->err = expand_word(&arith_expander);
+	if (expander->err.type)
+		return (expander_free(&arith_expander), expander->err);
+	expansion_init(&expansion);
+	expander->err = expansion_load(&expansion, &arith_expander.fields);
+	if (!expander->err.type)
+		compute_arith_expression(expander, &expansion);
+	expander_free(&arith_expander);
+	return (expansion_free(&expansion), expander->err);
 }
