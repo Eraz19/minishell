dfdf956144aea5f0dd38827f0fc951705590e5b3
SudoQuack <guillaume.astesana@gmail.com>
2026-07-09 15:45:16 +0200

    feat(builtins-export): dissociate error code and exit status

--- includes/builtins.h
+++ includes/builtins.h
@@ -1,51 +1,63 @@
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
+# include "error.h"
 # include <stdbool.h>
 
-typedef int (*t_builtin)(int argc, char **argv, char **envp);
+// @p exit_status ERR_NO / ERR_INVALID_USAGE / ERR_UB / ERR_BUILTIN / ERR_LIBC
+// @ret ERR_NO / ERR_BUILTIN / ERR_UB / ERR_INTERNAL / ERR_LIBC
+typedef t_error (*t_builtin)(int argc, char **argv, char **envp, int *exit_status);
 
 // Special builtins (Shell shall exit on error if invoked directly && non-interactive)
 
-int	builtin_break(int argc, char **argv, char **envp);
-int	builtin_colon(int argc, char **argv, char **envp);
-int	builtin_continue(int argc, char **argv, char **envp);
-int	builtin_dot(int argc, char **argv, char **envp);
-int	builtin_eval(int argc, char **argv, char **envp);
-int	builtin_exec(int argc, char **argv, char **envp);
-int	builtin_exit(int argc, char **argv, char **envp);
-int	builtin_export(int argc, char **argv, char **envp);
-int	builtin_readonly(int argc, char **argv, char **envp);
-int	builtin_return(int argc, char **argv, char **envp);
-int	builtin_set(int argc, char **argv, char **envp);
-int	builtin_shift(int argc, char **argv, char **envp);
-int	builtin_times(int argc, char **argv, char **envp);
-int	builtin_trap(int argc, char **argv, char **envp);
-int	builtin_unset(int argc, char **argv, char **envp);
+t_error	builtin_break(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_colon(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_continue(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_dot(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_eval(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_exec(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_exit(int argc, char **argv, char **envp, int *exit_status);
+
+// @p exit_status ERR_INVALID_USAGE / ERR_ASSIGNMENT_MISSING_NAME
+// 					/ ERR_VAR_INVALID_NAME / ERR_VAR_READ_ONLY
+// 					/ ERR_UB / ERR_INTERNAL / ERR_LIBC
+// @ret ERR_BUILTIN / ERR_UB / ERR_INTERNAL / ERR_LIBC
+t_error	builtin_export(int argc, char **argv, char **envp, int *exit_status);
+
+// TODO
+t_error	builtin_readonly(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_return(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_set(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_shift(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_times(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_trap(int argc, char **argv, char **envp, int *exit_status);
+
+// TODO
+t_error	builtin_unset(int argc, char **argv, char **envp, int *exit_status);
 
 // Intrinsic builtins (Shell shall not exit on error)
 
-int	builtin_alias(int argc, char **argv, char **envp);
-int	builtin_bg(int argc, char **argv, char **envp);
-int	builtin_cd(int argc, char **argv, char **envp);
-int	builtin_command(int argc, char **argv, char **envp);
-int	builtin_fc(int argc, char **argv, char **envp);
-int	builtin_fg(int argc, char **argv, char **envp);
-int	builtin_getopts(int argc, char **argv, char **envp);
-int	builtin_hash(int argc, char **argv, char **envp);
-int	builtin_jobs(int argc, char **argv, char **envp);
-int	builtin_kill(int argc, char **argv, char **envp);
-int	builtin_read(int argc, char **argv, char **envp);
-int	builtin_type(int argc, char **argv, char **envp);
-int	builtin_ulimit(int argc, char **argv, char **envp);
-int	builtin_umask(int argc, char **argv, char **envp);
-int	builtin_unalias(int argc, char **argv, char **envp);
-int	builtin_wait(int argc, char **argv, char **envp);
+t_error	builtin_alias(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_bg(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_cd(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_command(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_fc(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_fg(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_getopts(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_hash(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_jobs(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_kill(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_read(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_type(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_ulimit(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_umask(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_unalias(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_wait(int argc, char **argv, char **envp, int *exit_status);
 
 // Regular builtins (Shell shall not exit on error)
 
-int	builtin_echo(int argc, char **argv, char **envp);
-int	builtin_env(int argc, char **argv, char **envp);
-int	builtin_pwd(int argc, char **argv, char **envp);
+t_error	builtin_echo(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_env(int argc, char **argv, char **envp, int *exit_status);
+t_error	builtin_pwd(int argc, char **argv, char **envp, int *exit_status);
 
 #endif
