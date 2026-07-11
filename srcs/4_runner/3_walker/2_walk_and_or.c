11fa6499e4b7df63f48300c51caeeafd72ddf817
SudoQuack <guillaume.astesana@gmail.com>
2026-07-12 01:17:17 +0200

    feat(runner-walker): and_or handling

--- srcs/4_runner/3_walker/2_walk_and_or.c
+++ srcs/4_runner/3_walker/2_walk_and_or.c
@@ -5,17 +5,25 @@ t_error	walk_and_or(t_runner *runner, t_ast_and_or *and_or, int *exit_status)
 {
 	size_t			i;
 	t_ast_pipeline	*pipeline;
+	bool			next_on_success;
 	t_error			err;
 
 	i = 0;
+	*exit_status = 0;
+	err = error(ERR_NO);
 	while (i < and_or->pipelines.len)
 	{
-		// TODO: next_on_success
+		*exit_status = -1;
 		pipeline = &((t_ast_pipeline *)and_or->pipelines.data)[i];
 		err = walk_pipeline(runner, pipeline, exit_status);
-		if (err.type)
-			return (err);
+		if (err.type || i == and_or->pipelines.len - 1)
+			break ;
+		next_on_success = ((bool *)and_or->next_on_success.data)[i];
+		if (next_on_success == true && *exit_status != 0)
+			break ;
+		if (next_on_success == false && *exit_status == 0)
+			break ;
 		i++;
 	}
-	return (error(ERR_NO));
+	return (walk_normalize_output(err, NULL, exit_status));
 }
