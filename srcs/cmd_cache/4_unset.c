40126609fbbce30dda2470dcb508a4644da90e35
SudoQuack <guillaume.astesana@gmail.com>
2026-07-21 12:38:04 +0200

    feat(builtin): eval

--- srcs/cmd_cache/4_unset.c
+++
@@ -1,6 +0,0 @@
-#include "cmd_cache.h"
-
-void	cmd_cache_unset(t_cmd_cache *cache, const t_string *cmd_name)
-{
-	(void)hashmap_remove(cache, cmd_name->data);
-}
--- srcs/signal/0_sig_global_var.c
+++
@@ -1,3 +0,0 @@
-#include "sig_priv.h"
-
-t_signals	g_signals;
