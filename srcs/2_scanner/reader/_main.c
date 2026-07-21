54e7c42ad33334251a947dc02d71f387cd827dc0
Alexander Douieb <alexanderdouieb19@gmail.com>
2026-07-21 23:13:47 +0200

    fix scanner error on non interractive

--- srcs/2_scanner/reader/_main.c
+++ srcs/2_scanner/reader/_main.c
@@ -1,6 +1,7 @@
 #include "libft.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "options.h"
 #include "reader_.h"
 #include "history.h"
@@ -42,7 +43,7 @@ t_error	reader_continuation(t_string *res)
 	if (err.type)
 		return (err);
 	if (!is_interactive)
-		return (error(ERR_POSIX_SYNTAX));
+		return (error(ERR_NO));
 	(void)string_init(&continuation, 0, NULL, 0);
 	while (continuation.len < 2)
 	{
@@ -77,11 +78,13 @@ static t_error	reader_open_source(const char *path, int *fd)
 	return (err);
 }
 
+#include <stdio.h>
 t_error	reader_file_input(t_string *res, const char *path)
 {
 	int			fd;
 	t_error		err;
 
+	printf("read file input: path [%s]\n", path);
 	err = reader_open_source(path, &fd);
 	if (err.type)
 		return (err);
@@ -91,10 +94,12 @@ t_error	reader_file_input(t_string *res, const char *path)
 		err = reader_read_error(err, path);
 		return (error_priorize(err, posix_close_if_open(fd)));
 	}
+	printf("read file input: read [%zu] | [%s] bytes\n", res->len, res->data);
 	err = posix_close_if_open(fd);
 	if (err.type)
 		return (string_free(res), err);
 	if (!string_append_n(res, "\n", 1))
 		return (err = error_sys(), string_free(res), err);
+	printf("read file input: appended newline [%zu] | [%s] bytes\n", res->len, res->data);
 	return (err);
 }
